941a94da5d8ea5bee0dbc73aa5b728a9f32dcab0
kyutae1994 <k305059ute@gmail.com>
2023-04-25 16:42:03 +0900

    로그인 jwt 구현

--- src/main/java/gallery/back/art/backend/configuration/CustomerDetailsService.java
+++ src/main/java/gallery/back/art/backend/configuration/CustomerDetailsService.java
@@ -1,13 +1,15 @@
 package gallery.back.art.backend.configuration;
 
+import gallery.back.art.backend.entity.Member;
 import gallery.back.art.backend.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component("userDetailsService")
+@Service
 @RequiredArgsConstructor
 public class CustomerDetailsService implements UserDetailsService {
 
@@ -15,6 +17,6 @@ public class CustomerDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return (UserDetails) accountRepository.findByEmail(username);
+        return new CustomerDetails(accountRepository.findByEmail(username));
     }
 }
